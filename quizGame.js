050691eca31b612bd7809e3b68f4c008fee53b1e
Mitch4sho <mitchelloliver1991@gmail.com>
2020-09-15 07:55:30 -0700

    intial commit

---
+++ quizGame.js
@@ -0,0 +1,7 @@
+// question function constructor 
+
+var questions = function () {
+    this.question = '',
+        this.answer = [],
+        this.correct = ' '; // use a number to get the correct answer
+}
